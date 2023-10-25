0f38904ed267d688f6b2d596df4a65f166fbcece
Mathieu Savelli <mathieu.savelli@awakenweb.fr>
2023-10-25 10:10:36 +0200

    Replaced OHM with LHM and added new monitors

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# deckboard-ohm
-Display hardware status from Open Hardware Monitor on Deckboard button
+# deckboard-lhm
+Display hardware status from Libre Hardware Monitor on Deckboard button
+
+I replaced Open Hardware Monitor by its fork [Libre Hardware Monitor](https://github.com/LibreHardwareMonitor/LibreHardwareMonitor/releases), that works with newer hardware without breaking backward compatibility.
+
+**You must have Libre Hardware Monitor running in the background to display values.**
+
+
+![Example of buttons using the new monitors](/static/example.png)
+
+
+## Available monitors
+
+**Depending on your hardware, some values may not be available**
+
+* CPU Load
+* CPU Temperature
+* CPU Power consumption
+
+* GPU Load (two methods, one accurate but depending on the sensors available, and one using the Direct3D load)
+* GPU Core Temperature
+* GPU Hot Spot Temperature
+* GPU Power consumption
+* GPU Clock Speed
+* GPU Memory Speed
+
+* RAM Load
+
+* VRM Temperature
+
+____
+
+Tested on
+* Ryzen 5500U with integrated graphics laptop
+* Intel Core i3 4010U with integrated graphics laptop
+* Ryzen 5800X and NVidia 3060Ti on MSI B550 Motherboard _(the screenshot comes from this machine)_
--- index.js
+++ index.js
@@ -3,30 +3,54 @@ const { Query } = require('node-wmi');
 
 const CATEGORIZATION = {
 	load: {
-		'Memory': 'ram',
-		'CPU Total': 'cpu',
-		'GPU Core': 'gpu'
+		'Memory': 'ram', // Generic
+		'CPU Total': 'cpu', // Generic
+		'GPU Core': 'gpu', // NVidia
+		'D3D 3D': 'gpu-backup' // Best approximation if actual load is missing
 	},
 	temperature: {
-		'CPU Package': 'cpu',
-		'GPU Core': 'gpu'
+		'Core (Tctl/Tdie)': 'cpu', // AMD
+		'CPU Package': 'cpu', // Intel
+		'GPU Core': 'gpu', // NVidia
+		'GPU Hot Spot': 'hotspot-gpu', // NVidia
+		'VRM MOS': 'vrm' // from Motherboard if available
+	},
+	clock: {
+		'GPU Core': 'gpu', // NVidia
+		'GPU Memory': 'memory-gpu' // NVidia
+	},
+	power: {
+		'Package': 'cpu', // AMD
+		'CPU Package': 'cpu', // Intel
+		'GPU Power': 'gpu', // Intel iGPU
+		'GPU Package': 'gpu' // NVdia
 	}
 }
 
 const SUFFIX = {
 	load: '%',
-	temperature: '°C'
+	temperature: '°C',
+	clock: ' MHz',
+	power: ' W'
 }
 
 const DEFAULT_VALUE = {
 	'hw-load-cpu': '-%',
 	'hw-load-gpu': '-%',
+	'hw-load-gpu-backup': '-%',
 	'hw-load-ram': '-%',
 	'hw-temperature-cpu': '-°C',
-	'hw-temperature-gpu': '-°C'
+	'hw-temperature-gpu': '-°C',
+	'hw-temperature-hotspot-gpu': '-°C',
+	'hw-temperature-vrm': '-°C',
+	'hw-clock-gpu': '-MHz',
+	'hw-clock-memory-gpu': '-MHz',
+	'hw-power-cpu': '-W',
+	'hw-power-gpu': '-W'
+
 }
 
-class OpenHardwareMonitor extends Extension {
+class LibreHardwareMonitor extends Extension {
 	constructor(props) {
 		super(props);
 		this.setValue = props.setValue;
@@ -47,7 +71,8 @@ class OpenHardwareMonitor extends Extension {
 						type: INPUT_METHOD.INPUT_SELECT,
 						items: [
 							{ value: 'hw-load-cpu', label: 'CPU Load' },
-							{ value: 'hw-temperature-cpu', label: 'CPU Temperature' }
+							{ value: 'hw-temperature-cpu', label: 'CPU Temperature' },
+							{ value: 'hw-power-cpu', label: 'CPU Power Draw' }
 						]
 					}
 				]
@@ -65,10 +90,31 @@ class OpenHardwareMonitor extends Extension {
 						type: INPUT_METHOD.INPUT_SELECT,
 						items: [
 							{ value: 'hw-load-gpu', label: 'GPU Load' },
-							{ value: 'hw-temperature-gpu', label: 'GPU Temperature' }
+							{ value: 'hw-load-gpu-backup', label: 'GPU Load (backup solution)' },
+							{ value: 'hw-temperature-gpu', label: 'GPU Temperature' },
+							{ value: 'hw-temperature-hot-spot-gpu', label: 'GPU Hotspot Temperature' },
+							{ value: 'hw-power-gpu', label: 'GPU Power Draw' },
+							{ value: 'hw-clock-gpu', label: 'GPU Clock' },
+							{ value: 'hw-clock-memory-gpu', label: 'GPU Memory Clock' }
 						]
 					}
 				]
+			},
+			{
+				label: 'Display RAM Stats',
+				value: 'hw-load-ram',
+				icon: 'headphones',
+				mode: 'custom-value',
+				fontIcon: 'fas',
+				color: '#8E44AD'
+			},
+			{
+				label: 'Display VRM Stats',
+				value: 'hw-temperature-vrm',
+				icon: 'headphones',
+				mode: 'custom-value',
+				fontIcon: 'fas',
+				color: '#8E44AD'
 			}
 		];
 		this.configs = [];
@@ -78,22 +124,22 @@ class OpenHardwareMonitor extends Extension {
 	initExtension() {
 		if (process.platform === 'win32')
 			setInterval(() => {
-				Query()
-					.namespace('root/OpenHardwareMonitor')
-					.class('Sensor')
-					.where("SensorType='Load' OR SensorType='Temperature'")
-					.exec((err, data) => {
-						if (err || !data)
-							this.setValue(DEFAULT_VALUE)
-						else this.setValue(data
-							.filter(({ Name, SensorType }) => CATEGORIZATION[SensorType.toLowerCase()][Name] !== undefined)
-							.map(({ Name, Value, SensorType }) => ({
-								key: 'hw-' + SensorType.toLowerCase() + '-' + CATEGORIZATION[SensorType.toLowerCase()][Name],
-								value: Math.round(Value) + SUFFIX[SensorType.toLowerCase()]
-							}))
-							.reduce((data, { key, value }) => ({ ...data, [key]: value }), DEFAULT_VALUE)
-						)
-					})
+					Query()
+						.namespace('root/LibreHardwareMonitor')
+						.class('Sensor')
+						.where("SensorType ='Load' OR SensorType ='Temperature' OR SensorType = 'Clock' OR SensorType = 'Power'")
+						.exec((err, data) => {
+							if (err || !data)
+								this.setValue(DEFAULT_VALUE)
+							else this.setValue(data
+								.filter(({ Name, SensorType }) => CATEGORIZATION[SensorType.toLowerCase()][Name] !== undefined)
+								.map(({ Name, Value, SensorType }) => ({
+									key: 'hw-' + SensorType.toLowerCase() + '-' + CATEGORIZATION[SensorType.toLowerCase()][Name],
+									value: Math.round(Value) + SUFFIX[SensorType.toLowerCase()]
+								}))
+								.reduce((data, { key, value }) => ({ ...data, [key]: value }), DEFAULT_VALUE)
+							)
+						})
 			}, 2000)
 		else this.setValue(DEFAULT_VALUE)
 	}
@@ -103,4 +149,4 @@ class OpenHardwareMonitor extends Extension {
 	};
 }
 
-module.exports = (sendData) => new OpenHardwareMonitor(sendData);
+module.exports = (sendData) => new LibreHardwareMonitor(sendData);
